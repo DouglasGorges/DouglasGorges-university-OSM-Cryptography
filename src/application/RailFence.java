9fcd96a81b92cf93726069d2087441cc2b830a78
douglasgorges <douglasgorges@icloud.com>
2019-11-20 15:14:41 -0300

    Code cleanning

--- src/application/RailFence.java
+++ src/application/RailFence.java
@@ -1,18 +1,16 @@
 package application;
 
-import java.io.PrintStream;
 import java.util.Scanner;
 
 public class RailFence {
 
 	private static Scanner in;
 
-	public static void main(String arg[]) {
+	public static void main(String args[]) {
 
-		System.out.println("Create a key (only numbers):");
+		System.out.println("Create a key (some small numbers):");
 		in = new Scanner(System.in);
 		int key = in.nextInt();
-
 		System.out.println("Enter the text for encryption");
 		in = new Scanner(System.in);
 		String text = in.next();
@@ -29,29 +27,22 @@ public class RailFence {
 	}
 
 	public static void encrypt(String text, int key) {
-
 		StringBuilder encryptedText = new StringBuilder();
 		boolean goDown = false;
 		int j = 0;
 		int columns = text.length();
 		char[][] matrix = new char[key][columns];
-
 		for (int i = 0; i < columns; i++) {
-
 			if (j == (key - 1) || j == 0) {
 				goDown = !goDown;
 			}
-
 			matrix[j][i] = text.charAt(i);
-
 			if (goDown) {
 				j++;
-
 			} else {
 				j--;
 			}
 		}
-
 		for (int i = 0; i < key; i++) {
 			for (int k = 0; k < columns; k++) {
 				if (matrix[i][k] != 0) {
@@ -59,66 +50,48 @@ public class RailFence {
 				}
 			}
 		}
-
 		System.out.printf("Encrypted text: %s %n %n", encryptedText);
-
 	}
 
 	public static void decrypt(String str, int key) {
-
 		StringBuilder decryptedText = new StringBuilder();
 		boolean goDown = false;
 		int j = 0;
 		int columns = str.length();
 		int index = 0;
 		char[][] matrix = new char[key][columns];
-
 		for (int i = 0; i < columns; i++) {
 			if (j == (key - 1) || j == 0) {
 				goDown = !goDown;
 			}
-
 			matrix[j][i] = '*';
-
 			if (goDown) {
 				j++;
-
 			} else {
 				j--;
 			}
-
 		}
 
 		for (int i = 0; i < key; i++) {
 			for (int k = 0; k < columns; k++) {
-
 				if (matrix[i][k] == '*' && index < str.length()) {
 					matrix[i][k] = str.charAt(index++);
-
 				}
 			}
 		}
-
 		goDown = false;
 		j = 0;
-
 		for (int i = 0; i < columns; i++) {
 			if (j == (key - 1) || j == 0) {
 				goDown = !goDown;
 			}
-
 			decryptedText.append(matrix[j][i]);
-
 			if (goDown) {
 				j++;
-			}
-
-			else {
+			} else {
 				j--;
 			}
 		}
-
 		System.out.printf("Decrypted text: %s %n %n", decryptedText);
-
 	}
 }
